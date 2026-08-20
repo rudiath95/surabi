d6a8fb3635ac7b6d2d061d579b94e37c537e4d6a
rudiath95 <rudiath.r@gmail.com>
2026-08-20 16:06:20 +0700

    Add Vue 3, Tailwind CSS, and Font Awesome setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center gap-4 bg-gray-100">
+    <i class="fa-solid fa-seedling text-4xl text-green-600"></i>
+    <h1 class="text-3xl font-bold text-gray-900">Surabi</h1>
+    <p class="text-gray-600">Vue 3 + Tailwind CSS + Font Awesome</p>
+  </div>
+</template>
